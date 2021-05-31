85820a21aa765d394bd9a71a48543819757c74f2
Lucas Borges <76432049+DevLucasBorges@users.noreply.github.com>
2021-05-31 09:32:04 -0300

    Add files via upload

---
+++ heritage III/src/entities/BusinessAccount.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class BusinessAccount {
+
+	public BusinessAccount(int i, String string, double d, double e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void loan(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ heritage III/src/entities/SavingsAccount.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class SavingsAccount {
+
+	public SavingsAccount(int i, String string, double d, double e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateBalance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
